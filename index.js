2fc9610ca978efcb6b75f947b4b6d621d07d7a58
Judre <Reyesjudre@gmail.com>
2020-11-22 16:44:08 -0800

    Completed for HW12

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
 // Add required packages
 const express = require("express");
 const app = express();
-
-
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+const multer = require("multer");
+const upload = multer();
 
 // Set up EJS
 app.set("view engine", "ejs");
@@ -34,15 +35,69 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/input", (req, res) => {
+    res.render("input");
+ });
+ 
+ app.post("/input", (req, res) => {
+     if(!req.files || Object.keys(req.files).length === 0) {
+         message = "Error: Import file not uploaded";
+         return res.send(message);
+     };
+     //Read file line by line, inserting records
+     const fn = req.files.filename;
+     const buffer = fn.data;
+     const lines = buffer.toString().split(/\r?\n/);
+ 
+     lines.forEach(line => {
+          //console.log(line);
+          product = line.split(",");
+          //console.log(product);
+          const sql = "INSERT INTO PRODUCT(prod_id, prod_name, prod_desc, prod_price) VALUES ($1, $2, $3, $4)";
+          pool.query(sql, product, (err, result) => {
+              if (err) {
+                  console.log(`Insert Error.  Error message: ${err.message}`);
+              } else {
+                  console.log(`Inserted successfully`);
+              }
+         });
+     });
+     message = `Processing Complete - Processed ${lines.length} records`;
+     res.send(message);
+ });   
+
+ app.get("/output", (req, res) => {
+    var message = "";
+    res.render("output",{ message: message });
+   });
+   
+   
+   app.post("/output", (req, res) => {
+       const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
+       pool.query(sql, [], (err, result) => {
+           var message = "";
+           if(err) {
+               message = `Error - ${err.message}`;
+               res.render("output", { message: message })
+           } else {
+               var output = "";
+               result.rows.forEach(product => {
+                   output += `${product.prod_id},${product.prod_name},${product.prod_desc},${product.prod_price}\r\n`;
+               });
+               res.header("Content-Type", "text/csv");
+               res.attachment("export.csv");
+               return res.send(output);
+           };
+       });
+   });
+
 // Add database package and connection string (can remove ssl)
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL= "postgres://vwkljkaetbgmnb:d9df2dff2d66995c78d0bc8ef4ea86635bcead24ed61c83308e1706a3b8cfb3c@ec2-54-85-13-135.compute-1.amazonaws.com:5432/d9qenh3u5baut5",
+  connectionString: process.env.DATABASE_URL="postgres://vwkljkaetbgmnb:d9df2dff2d66995c78d0bc8ef4ea86635bcead24ed61c83308e1706a3b8cfb3c@ec2-54-85-13-135.compute-1.amazonaws.com:5432/d9qenh3u5baut5",
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-
-
-
+require('dotenv').config()
